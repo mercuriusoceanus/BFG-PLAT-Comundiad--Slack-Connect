d4795e65400abffdc9992c65cbf9e43f2a2853f4
sonidiaz <nachodiaz8@gmail.com>
2021-01-31 19:21:23 +0100

    Proof to merge

--- bfg-slack-front/src/services/getAccessToken.js
+++ bfg-slack-front/src/services/getAccessToken.js
@@ -6,7 +6,6 @@ async function getAccessToken(code, idSlack, secretSlack ) {
         'Content-Type': 'application/x-www-form-urlencoded',
       },
       body: `code=${code}&client_id=1202804016946.1695908297266&client_secret=78db4d92c56ad88e92a4b1828a89c74f`,
-      // body: `code=${code}&client_id=137723501795.1353738797012&client_secret=79472285d2bcdd5c24b56e09006be4ee`,
     });
     const data = await resp.json();
     return data;
